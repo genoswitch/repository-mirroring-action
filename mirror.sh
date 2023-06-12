a76d61cd1d28ee9e2ab5b6b19cc825736f02b38d
James Cahill <jcahill773@gmail.com>
2023-06-12 07:26:54 +0100

    (mirror): Add current directory as a safe dir

--- mirror.sh
+++ mirror.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 set -eu
 
+# Git 2.30.23+ will not exec commands until the directory is "trusted"
+# https://github.com/git/git/commit/8959555cee7ec045958f9b6dd62e541affb7e7d9
+git config --global --add safe.directory $(pwd)
+
 /setup-ssh.sh
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
